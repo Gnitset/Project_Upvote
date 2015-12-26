ad01da783d128eac21aa3b24bba3eb6e10b3d8e0
Klas Meder Boqvist <github.com@servern.info>
2015-12-26 04:42:37 +0100

    Adding loop and cheking credits

--- Upvote.py
+++ Upvote.py
@@ -29,6 +29,8 @@ user = raw_input("Enter username to upvote: ")
 total_comments = client.get_account_comment_count(user)
 comment_pages=total_comments/50
 
+print "Trying to upvote %s comments with following limits: %s"%(total_comments, client.credits)
+
 comments=list()
 comments_full = comments
 
@@ -37,20 +39,30 @@ for Z in range(0,comment_pages+1):
 
 co_d=dict()
 
-for idx,comment in enumerate(comments):
-	try:
-		if not co_d.has_key(comment):
-			co_d[comment] = client.get_comment(comment)
-		if co_d[comment].vote != u'up':
-			client.comment_vote(comment, vote='up')
-			print "Upvoted comment id: %s, idx: %s"%(comment, idx)
-		else:
-			print "Comment already upvoted id: %s, idx: %s"%(comment, idx)
-		comments.remove(comment)
-	except ImgurClientError as error:
-		time.sleep(0.5)
-		print "Request failed for comment id: %s, idx: %s"%(comment, idx)
-	except TypeError as te:
-		print "Creating comment-object failed for comment id: %s, idx: %s"%(comment, idx)
-
+lengths=(len(comments), len(co_d))
+while comments:
+	print "Comments to process: %s. Credits: %s"%(str(lengths), str(client.credits))
+	if client.credits['UserRemaining'] != None and len(comments) > int(client.credits['UserRemaining']):
+		print "Breaking due to limits: %s"%str(client.credits)
+		break
+	for idx,comment in enumerate(comments):
+		try:
+			if not co_d.has_key(comment):
+				co_d[comment] = client.get_comment(comment)
+			if co_d[comment].vote != u'up':
+				client.comment_vote(comment, vote='up')
+				print "Upvoted comment id: %s, idx: %s"%(comment, idx)
+			else:
+				print "Comment already upvoted id: %s, idx: %s"%(comment, idx)
+			comments.remove(comment)
+		except ImgurClientError as error:
+			time.sleep(0.5)
+			print "Request failed for comment id: %s, idx: %s"%(comment, idx)
+		except TypeError as te:
+			print "Creating comment-object failed for comment id: %s, idx: %s"%(comment, idx)
+	new_lengths=(len(comments), len(co_d))
+	if new_lengths == lengths:
+		break
+	else:
+		lengths = new_lengths
 print client.credits
